8b70813301ab67816a0403dffeb66089a9d3318f
Julian Weiss <me@insanj.com>
2022-05-01 09:55:20 -0400

    start tracking dist/

---
+++ dist/OogyDancing.d.ts
@@ -0,0 +1,39 @@
+export declare module OogyDancing {
+    interface OogyDancingAPI {
+        readonly options: OogyDancingOptions;
+        readonly attachedElement?: Element;
+        attach(element: Element): void;
+    }
+    type OogyDancingOptions = {
+        kind: OogyDancingKind;
+        duration: number;
+        width: number;
+        height: number;
+        distance: number;
+        horizontal: OogyDancingAlign;
+        horizontalInset: number;
+        vertical: OogyDancingAlign;
+        verticalInset: number;
+    };
+    enum OogyDancingKind {
+        carrot = "carrot"
+    }
+    enum OogyDancingAlign {
+        start = "flex-start",
+        center = "center",
+        end = "flex-end"
+    }
+    const kOogyDancingOptionsDefault: OogyDancingOptions;
+    class OogyDancer implements OogyDancingAPI {
+        get options(): OogyDancingOptions;
+        get attachedElement(): Element | undefined;
+        private _options;
+        private subelement;
+        private dancingElement;
+        constructor(options?: OogyDancingOptions);
+        private readonly kOogyDancingCarrotBackgroundImage;
+        private readonly kOogyDancingCarrotAnimationName;
+        private prepare;
+        attach(element: Element): void;
+    }
+}
